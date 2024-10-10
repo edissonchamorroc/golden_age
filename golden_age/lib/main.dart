f6a4cb85088b693238f5de10d8a60a1fe7f11102
edissonchamorroc <john.chamorro@udea.edu.co>
2024-10-10 18:37:16 -0500

    Se realiza configuracion de firebase en el proyecto. Se crea modelo de usuario y folder repository para conectar con fire auth

--- golden_age/lib/main.dart
+++ golden_age/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:golden_age/firebase_options.dart';
 import 'package:golden_age/pages/login_pages.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
---
+++ golden_age/repository/firbase_api.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirbaseApi {
+  Future<String?> createUser(String emailAddress, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return credential.user?.uid;
+    } on FirebaseAuthException catch (e) {
+      print('Firebase auth exception ${e.code}');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String?> signInUser(String emailAddress, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
+      return credential.user?.uid;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+}
