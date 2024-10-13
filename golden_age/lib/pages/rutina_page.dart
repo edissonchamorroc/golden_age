3bbc851631f702ca03d849f165e7ba18f1045656
edissonchamorroc <john.chamorro@udea.edu.co>
2024-10-13 10:23:29 -0500

    se crean paginas, solo base, rutina y seguimiento para trabajar en navigation bar

---
+++ golden_age/lib/pages/rutina_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RutinaPage extends StatefulWidget {
+  const RutinaPage({super.key});
+
+  @override
+  State<RutinaPage> createState() => _RutinaPageState();
+}
+
+class _RutinaPageState extends State<RutinaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("Rutina page"),
+    );
+  }
+}
